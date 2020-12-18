8c4fddf37ea277a8f4c7165a9f1e58e4f30ac7b8
Rosie O'Donnell <rosie.odonnell@talk21.com>
2020-12-18 08:54:46 +0000

    adding back in the popUp() function and making click button back-and-forth

--- javascript.js
+++ javascript.js
@@ -12,8 +12,8 @@ function languageClick() {
     var english = document.getElementsByClassName("english");
     english[0]["innerText"] = "ROSIE O'DONNELL";
     english[1]["innerText"] = "Acerca de mí";
-    english[2]["innerText"] = "Aqui estara la traduccion";
-    
+    english[2]["innerText"] = "Aquí estara la traduccion";
+
     if (buttonText == "EN") {
         languageButton[0]["innerText"] = "ES";
         english[0]["innerText"] = "ROSIE O'DONNELL";
@@ -26,14 +26,26 @@ function languageClick() {
 //ABOUT ME FUNCTION:
 
 function popUp(){
+    var aboutMeTest = document.getElementById("about-me");
+    console.log(aboutMeTest);
     const newDiv = document.createElement("div"); //tag type anchor
     const newContent = document.createTextNode("This section is going to tell you all about me and why I am applying for this course.");
     newDiv.appendChild(newContent);
-    newDiv.id = "about-me-box-2";
-    newDiv.className = "english";
-    const currentDiv = document.getElementById("about-me-box-1");
-    const insert = document.getElementById("quarter-two");
+    newDiv.id = "about-me";
+    newDiv.className = "english-div";
+    const currentDiv = document.getElementById("about-me-heading");
+    const insert = document.getElementById("me");
+    if (aboutMeTest == null) {
     insert.insertBefore(newDiv, currentDiv); //(what I want, what I have: anchor)
+    }
+    else {
+        var removeEl = document.getElementById("about-me");
+        removeEl.remove();
+        //insert.removeChild(newDiv);
+
+
+        //I THINK I NEED TO ADD ANOTHER IF STATEMENT THAT SAYS = IF "ABOUT ME"[INNER-TEXT] = "ACERCA DE MI", THEN CREATE ELEMENT IN SPANSIH, ELSE JUST DO THIS FUNCTION
+    }
     
 }
 
