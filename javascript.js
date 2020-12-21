f7f15963135585b2d36ba4560ce5cf556b4cb173
Rosie O'Donnell <rosie.odonnell@talk21.com>
2020-12-21 21:07:24 +0000

    adding in appear/disappear function for skills grid

--- javascript.js
+++ javascript.js
@@ -14,6 +14,12 @@ function languageClick() {
     english[1]["innerText"] = "Acerca de mí";
     //english[2]["innerText"] = "Aquí estara la traduccion";
 
+    // var element = document.getElementsById("about-me")
+    // if (element != null) {
+    //     element["innerText"] = "Aquí estara la traduccion";
+
+    // }
+
     if (buttonText == "EN") {
         languageButton[0]["innerText"] = "ES";
         english[0]["innerText"] = "ROSIE O'DONNELL";
@@ -21,6 +27,7 @@ function languageClick() {
         //english[2]["innerText"] = "This section is going to tell you all about me and why I am applying for this course.";
     
     };
+   
 }
 
 // add in this function, if AboutMeTest == null - add in translation or don't
@@ -50,6 +57,51 @@ function popUp(){
     }
     
 }
+
+function popUp2() {
+    var iconTest = document.getElementById("icon-1")
+    var img = document.createElement("img");
+    img.src = "javascript.png";
+    img.id = "icon-1";
+    var currentDiv2 = document.getElementById("my-skills");
+    var insert2 = document.getElementById("test");
+    currentDiv2.appendChild(img);
+
+    var iconTest2 = document.getElementById("icon-2")
+    var img2 = document.createElement("img");
+    img2.src = "css3.png";
+    img2.id = "icon-2";
+    var currentDiv2 = document.getElementById("my-skills");
+    var insert2 = document.getElementById("test");
+    currentDiv2.appendChild(img2);
+
+    var iconTest3 = document.getElementById("icon-3")
+    var img3 = document.createElement("img");
+    img3.src = "html5.png";
+    img3.id = "icon-3";
+    var currentDiv2 = document.getElementById("my-skills");
+    var insert2 = document.getElementById("test");
+    currentDiv2.appendChild(img3);
+    // add in the other elements here
+
+    //get this working below!
+
+if (iconTest == null && iconTest2 == null && iconTest2 == null) {
+    insert2.insertBefore(img, currentDiv2); 
+    //(what I want, what I have: anchor) //change this back to currentDiv
+    }
+    else {
+        var removeEl = document.getElementById("icon-1");
+        removeEl.remove();
+        var removeEl2 = document.getElementById("icon-2");
+        removeEl2.remove();
+        var removeEl3 = document.getElementById("icon-3");
+        removeEl3.remove();
+    }
+   
+    // create the div (containing divs, then make it a grid in CSS)
+}
+
 
 
 //document.body.onload = addElement;
