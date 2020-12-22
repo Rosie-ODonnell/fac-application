b640192d81948d70627c2930b2b285426154cdc5
Rosie O'Donnell <rosie.odonnell@talk21.com>
2020-12-22 18:19:01 +0000

    fixing popUp2() function and adding translation to popUp() function. Widening height of boxes and adding in media queries

--- javascript.js
+++ javascript.js
@@ -12,10 +12,11 @@ function languageClick() {
     var english = document.getElementsByClassName("english");
     english[0]["innerText"] = "ROSIE O'DONNELL";
     english[1]["innerText"] = "Acerca de mí";
+    english[2]["innerText"] = "Hazme click";
     //english[2]["innerText"] = "Aquí estara la traduccion";
 
-    // var element = document.getElementsById("about-me")
-    // if (element != null) {
+    //var element = document.getElementsById("about-me")
+    // if (element == null) {
     //     element["innerText"] = "Aquí estara la traduccion";
 
     // }
@@ -24,6 +25,7 @@ function languageClick() {
         languageButton[0]["innerText"] = "ES";
         english[0]["innerText"] = "ROSIE O'DONNELL";
         english[1]["innerText"] = "About Me";
+        english[2]["innerText"] = "Click me";
         //english[2]["innerText"] = "This section is going to tell you all about me and why I am applying for this course.";
     
     };
@@ -39,7 +41,16 @@ function popUp(){
     console.log(aboutMeTest);
     const newDiv = document.createElement("div"); //tag type anchor
     const newContent = document.createTextNode("This section is going to tell you all about me and why I am applying for this course.");
+    const newContentSpanish = document.createTextNode("aqui estara la traduccion");
+
+    // Getting the translation button to work:
+    const spanishTest = document.getElementById("about-me-heading");
+    if (spanishTest["innerText"] == "About Me") {
     newDiv.appendChild(newContent);
+    } else {
+        newDiv.appendChild(newContentSpanish);
+    }
+
     newDiv.id = "about-me";
     newDiv.className = "english";
     const currentDiv = document.getElementById("about-me-heading");
@@ -51,44 +62,40 @@ function popUp(){
         var removeEl = document.getElementById("about-me");
         removeEl.remove();
         //insert.removeChild(newDiv);
-
-
-        //I THINK I NEED TO ADD ANOTHER IF STATEMENT THAT SAYS = IF "ABOUT ME"[INNER-TEXT] = "ACERCA DE MI", THEN CREATE ELEMENT IN SPANSIH, ELSE JUST DO THIS FUNCTION
     }
-    
 }
 
+
+//MY SKILLS FUNCTION:
+
 function popUp2() {
-    var iconTest = document.getElementById("icon-1")
-    var img = document.createElement("img");
+    var iconTest = document.getElementById("icon-1");
+    const img = document.createElement("img");
     img.src = "javascript.png";
     img.id = "icon-1";
-    var currentDiv2 = document.getElementById("my-skills");
-    var insert2 = document.getElementById("test");
-    currentDiv2.appendChild(img);
+    var currentDiv2 = document.getElementById("test");
+    var insert2 = document.getElementById("my-skills");
+
 
     var iconTest2 = document.getElementById("icon-2")
     var img2 = document.createElement("img");
     img2.src = "css3.png";
     img2.id = "icon-2";
-    var currentDiv2 = document.getElementById("my-skills");
-    var insert2 = document.getElementById("test");
-    currentDiv2.appendChild(img2);
+    
 
     var iconTest3 = document.getElementById("icon-3")
     var img3 = document.createElement("img");
     img3.src = "html5.png";
     img3.id = "icon-3";
-    var currentDiv2 = document.getElementById("my-skills");
-    var insert2 = document.getElementById("test");
-    currentDiv2.appendChild(img3);
+   
     // add in the other elements here
 
-    //get this working below!
 
-if (iconTest == null && iconTest2 == null && iconTest2 == null) {
-    insert2.insertBefore(img, currentDiv2); 
-    //(what I want, what I have: anchor) //change this back to currentDiv
+if (iconTest == null && iconTest2 == null && iconTest3 == null) {
+    insert2.insertBefore(img, null); 
+    insert2.insertBefore(img2, null); 
+    insert2.insertBefore(img3, null); 
+    
     }
     else {
         var removeEl = document.getElementById("icon-1");
