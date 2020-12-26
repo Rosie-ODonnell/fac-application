5fd817376d5a3affad5712a47038baca38232556
Rosie O'Donnell <rosie.odonnell@talk21.com>
2020-12-26 15:36:19 +0000

    getting the translation button working with popUp() function! git status

--- javascript.js
+++ javascript.js
@@ -5,31 +5,34 @@ function languageClick() {
     var languageButton = document.getElementsByTagName("button");
     var english = document.getElementsByClassName("english");
     
-   
     var buttonText = languageButton[0]["innerText"];
     languageButton[0]["innerText"] = "EN";
 
     var english = document.getElementsByClassName("english");
-    english[0]["innerText"] = "HOLA! \n SOY ROSIE. \n BIENVENIDA A MI APLICACION.";
+    english[0]["innerText"] = "HOLA! \n SOY ROSIE. \n BIENVENIDO A MI SOLICITUD.";
     english[1]["innerText"] = "Acerca de mí";
     english[2]["innerText"] = "Hazme click";
-    //english[2]["innerText"] = "Aquí estara la traduccion";
-
-    //var element = document.getElementsById("about-me")
-    // if (element == null) {
-    //     element["innerText"] = "Aquí estara la traduccion";
-
-    // }
 
     if (buttonText == "EN") {
         languageButton[0]["innerText"] = "ES";
         english[0]["innerText"] = "HEY! \n I'M ROSIE. \n WELCOME TO MY APPLICATION.";
         english[1]["innerText"] = "About Me";
         english[2]["innerText"] = "Click me";
-        //english[2]["innerText"] = "This section is going to tell you all about me and why I am applying for this course.";
-    
+
     };
    
+
+    //this should translate the bit if it is already open
+    var element = document.getElementById("about-me")
+    if (element != null) {
+        element["innerText"] = "¡Me encanta la comida! Mis dos chefs favoritos son Meera Sodha (echa un vistazo a su columna vegana aquí) y Nigella Lawson. Si eres fan de Nigella, quizás te hayas dado cuenta de que mi esquema de colores está inspirado en su nuevo libro, Cook, Eat, Repeat. Prueba estos garabatos de rubí... ¡no decepcionarán! Me encanta el agua. Mi orden de preferencia por los destinos de natación es";
+    }
+
+    if (element != null && buttonText == "EN") {
+    element["innerText"] = "I 💕 food!  My two favourite chefs are Meera Sodha (check out her vegan column here) and Nigella Lawson. If you’re a Nigella fan, you might have noticed that my colour scheme is inspired by her new book, Cook, Eat, Repeat. Try these Ruby Noodles… they won’t disappoint! \n I love water. My order of preference for swimming destinations are: \n A calm sea > a clear lake > a river > a stream > a bath (alright, just for a soak) \n  If I can’t have those I’ll settle for London Fields Lido (yes, even in the winter)  \n  … not this Winter though! This year’s changed things a bit and I’ve ditched my goggles for pedals. I’m still at stage ‘all the gear, no idea’. \n I love dramatic sunsets and sunrises (you'll find some of the best I've seen at the bottom of this page).\n I love languages - When I was 23, I moved to Spain to learn Spanish. I didn’t know much when I went, but now I’m fluent! \n   I’m passionate about ending homelessness. I currently work as a caseworker for a homeless charity, and have been volunteering within the sector for the last 7 years. Now, I want to harness my capability to make a broader social impact.";
+
+}
+
 }
 
 // add in this function, if AboutMeTest == null - add in translation or don't - THIS IS NECESSARY - DO THIS!
@@ -41,18 +44,18 @@ function popUp(){
     console.log(aboutMeTest);
     const newDiv = document.createElement("div"); //tag type anchor
     const newContent = document.createTextNode(" I 💕 food!  My two favourite chefs are Meera Sodha (check out her vegan column here) and Nigella Lawson. If you’re a Nigella fan, you might have noticed that my colour scheme is inspired by her new book, Cook, Eat, Repeat. Try these Ruby Noodles… they won’t disappoint! \n I love water. My order of preference for swimming destinations are: \n A calm sea > a clear lake > a river > a stream > a bath (alright, just for a soak) \n  If I can’t have those I’ll settle for London Fields Lido (yes, even in the winter)  \n  … not this Winter though! This year’s changed things a bit and I’ve ditched my goggles for pedals. I’m still at stage ‘all the gear, no idea’. \n I love dramatic sunsets and sunrises (you'll find some of the best I've seen at the bottom of this page).\n I love languages - When I was 23, I moved to Spain to learn Spanish. I didn’t know much when I went, but now I’m fluent! \n   I’m passionate about ending homelessness. I currently work as a caseworker for a homeless charity, and have been volunteering within the sector for the last 7 years. Now, I want to harness my capability to make a broader social impact. ");
-    const newContentSpanish = document.createTextNode("aqui estara la traduccion");
-
+    const newContentSpanish = document.createTextNode("¡Me encanta la comida! Mis dos chefs favoritos son Meera Sodha (echa un vistazo a su columna vegana aquí) y Nigella Lawson. Si eres fan de Nigella, quizás te hayas dado cuenta de que mi esquema de colores está inspirado en su nuevo libro, Cook, Eat, Repeat. Prueba estos garabatos de rubí... ¡no decepcionarán! Me encanta el agua. Mi orden de preferencia por los destinos de natación es");
+    // newDiv.appendChild(newContent);
     // Getting the translation button to work:
     const spanishTest = document.getElementById("about-me-heading");
-    if (spanishTest["innerText"] == "HERE WILL BE AN A NICE PIC") {
-    newDiv.appendChild(newContent);
-    } else {
+    if (spanishTest["innerText"] == "Acerca de mí") {
         newDiv.appendChild(newContentSpanish);
+    } else {
+        newDiv.appendChild(newContent);
     }
 
     newDiv.id = "about-me";
-    newDiv.className = "english";
+    newDiv.className = "englishCreated";
     const currentDiv = document.getElementById("about-me-heading");
     const insert = document.getElementById("me");
     if (aboutMeTest == null) {
@@ -61,7 +64,6 @@ function popUp(){
     else {
         var removeEl = document.getElementById("about-me");
         removeEl.remove();
-        //insert.removeChild(newDiv);
     }
 }
 //THERE IS AN ISSUE UP HERE THAT ONCE IT HAS TRANSLATED TO SPANISH IT DOESN'T GO BACK. I THINK I NEED TO FIX THIS IN THE TRANSLATION FUNCTION
@@ -71,25 +73,24 @@ function popUp(){
 function popUp2() {
     var iconTest = document.getElementById("icon-1");
     const img = document.createElement("img");
-    img.src = "javascript.png";
+    img.src = "css3.png"; 
     img.id = "icon-1";
-    img.alt = "Javascript"
-    var currentDiv2 = document.getElementById("test");
+    img.alt = "CSS 3";
     var insert2 = document.getElementById("my-skills-input");
 
 
     var iconTest2 = document.getElementById("icon-2")
     var img2 = document.createElement("img");
-    img2.src = "css3.png";
+    img2.src = "javascript.png";
     img2.id = "icon-2";
-    img2.alt = "CSS 3"
+    img2.alt = "Javascript";
     
 
     var iconTest3 = document.getElementById("icon-3")
     var img3 = document.createElement("img");
     img3.src = "html5.png";
     img3.id = "icon-3";
-    img3.alt = "HTML 5"
+    img3.alt = "HTML 5";
    
     // add in the other elements here
 
@@ -108,19 +109,6 @@ if (iconTest == null && iconTest2 == null && iconTest3 == null) {
         var removeEl3 = document.getElementById("icon-3");
         removeEl3.remove();
     }
-   
-    // create the div (containing divs, then make it a grid in CSS)
+
 }
 
-
-
-//document.body.onload = addElement;
-
-function practice() {
-    const hiddenText = document.getElementsByClassName("practice");
-    if (hiddenText.style.display == "none") {
-        hiddenText.style.display = "block";
-    } else {
-        hiddenText.style.display = "none";
-    }
-}
