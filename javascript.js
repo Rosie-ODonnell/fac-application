d5334ab3309a1ceaaee9668a8c2ba42154b5662d
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-01-01 15:57:35 +0000

    changes to CSS and changing onclick function to event listeners

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 // LANGUAGE BUTTON FUNCTION: 
 
 function languageClick() {
@@ -16,13 +17,12 @@ function languageClick() {
     if (buttonText == "EN") {
         languageButton[0]["innerText"] = "ES";
         english[0]["innerText"] = "HEY! \n I'M ROSIE. \n WELCOME TO MY APPLICATION.";
-        english[1]["innerText"] = "About Me";
+        english[1]["innerText"] = "I'm a self-taught developer, applying to Founders and Coders Spring 2021 cohort to turn my dream of being a professional programmer into a reality! I've been coding for around 9 months and I love it! In fact, I’d go so far to say I’m obsessed, which I think is a pretty cool way to start a career.<br> But before I tell you why I'm applying for FAC I'd like to tell you a little bit about my non-computing side...";
         english[2]["innerText"] = "Click me";
 
     };
-   
 
-    //this should translate the bit if it is already open
+    //this will translate the About Me texted if it is already open:
     var element = document.getElementById("about-me")
     if (element != null) {
         element["innerText"] = "¡Me encanta la comida! Mis dos chefs favoritos son Meera Sodha (echa un vistazo a su columna vegana aquí) y Nigella Lawson. Si eres fan de Nigella, quizás te hayas dado cuenta de que mi esquema de colores está inspirado en su nuevo libro, Cook, Eat, Repeat. Prueba estos garabatos de rubí... ¡no decepcionarán! Me encanta el agua. Mi orden de preferencia por los destinos de natación es";
@@ -32,10 +32,10 @@ function languageClick() {
     element["innerText"] = "I 💕 food!  My two favourite chefs are Meera Sodha (check out her vegan column here) and Nigella Lawson. If you’re a Nigella fan, you might have noticed that my colour scheme is inspired by her new book, Cook, Eat, Repeat. Try these Ruby Noodles… they won’t disappoint! \n I love water. My order of preference for swimming destinations are: \n A calm sea > a clear lake > a river > a stream > a bath (alright, just for a soak) \n  If I can’t have those I’ll settle for London Fields Lido (yes, even in the winter)  \n  … not this Winter though! This year’s changed things a bit and I’ve ditched my goggles for pedals. I’m still at stage ‘all the gear, no idea’. \n I love dramatic sunsets and sunrises (you'll find some of the best I've seen at the bottom of this page).\n I love languages - When I was 23, I moved to Spain to learn Spanish. I didn’t know much when I went, but now I’m fluent! \n   I’m passionate about ending homelessness. I currently work as a caseworker for a homeless charity, and have been volunteering within the sector for the last 7 years. Now, I want to harness my capability to make a broader social impact.";
 
 }
-
 }
 
-// add in this function, if AboutMeTest == null - add in translation or don't - THIS IS NECESSARY - DO THIS!
+const lang = document.getElementById("language-button");
+lang.addEventListener('click', languageClick);
 
 //ABOUT ME FUNCTION:
 
@@ -66,7 +66,9 @@ function popUp(){
         removeEl.remove();
     }
 }
-//THERE IS AN ISSUE UP HERE THAT ONCE IT HAS TRANSLATED TO SPANISH IT DOESN'T GO BACK. I THINK I NEED TO FIX THIS IN THE TRANSLATION FUNCTION
+
+const me = document.getElementById("about-me-button");
+me.addEventListener('click', popUp);
 
 //MY SKILLS FUNCTION:
 
@@ -122,6 +124,8 @@ if (iconTest == null && iconTest2 == null && iconTest3 == null && iconTest4 == n
     }
 
 }
+const skills = document.getElementById("skills-button");
+skills.onclick = popUp2;
 
-// EVENT LISTENERS:
+
 
